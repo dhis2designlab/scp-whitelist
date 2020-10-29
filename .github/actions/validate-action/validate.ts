e033c18b5349dd2f3ef6311773318f2c542d8ace
goudbes <51861776+goudbes@users.noreply.github.com>
2020-10-29 13:53:40 +0100

    error fix

--- .github/actions/validate-action/validate.ts
+++ .github/actions/validate-action/validate.ts
@@ -10,7 +10,7 @@ const run = async (): Promise<void> => {
       core.setFailed(`Validate action failure`);
     }
 
-    if (payload.changed_files !== 1) {
+    if (payload.changed_files != 1) {
       console.error("More than one file has been changed.");
       core.setFailed(`Validate action failure`);
     }
